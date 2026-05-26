30460315670f32e6add2d9f3c16ad4a19103b73e
Thomas Livesey <tl1@translation0.xcmmc.nhs.uk>
2026-05-26 13:34:30 +0100

    Undone change to looksup.py suggested by coderabbit - fixed VEP consequence buttons

--- main/lookups.py
+++ main/lookups.py
@@ -137,15 +137,14 @@ def get_variants(search_key: str, search_value: str) -> list:
         is_snv = len(db_variant.ref) == len(db_variant.alt) == 1
         # Construct variant dict which keys matches variant table 
         # "data-field" properties in "variants.html" template.
-        wst_consequence = get_worst_csq_display_term(db_variant.consequence)
         variant = {
             'variant_id': db_variant.id,
             'chrom': db_variant.chrom,
             'pos': db_variant.pos,
             'allele_type': 'SNV' if is_snv else 'INDEL',
-            'consequence': wst_consequence,
+            'consequence': get_worst_csq_display_term(db_variant.consequence),
             'consequence_category': get_consequence_category(
-                wst_consequence, db_variant.hgvs_p
+                db_variant.consequence, db_variant.hgvs_p
             ),
             'hgvs_c': _format_hgvs(db_variant.hgvs_c),
             'hgvs_p': _format_hgvs(db_variant.hgvs_p),
